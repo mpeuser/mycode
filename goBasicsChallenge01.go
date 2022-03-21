fa3e7b391313567c5407f9d661f19be2f2aae0b9
Marcel Peuser <mpeuser>
2022-03-21 10:38:22 +0000

    learning something

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,13 @@
+/* Alta3 Research | RZFeeser
+   Multiple functions   */
+
+package main
+
+import "fmt"
+
+
+func main() {
+    fmt.Println("Marcel Peuser")    // we'll learn about inputting data later
+    fmt.Println("World capitol Duisburg")
+    fmt.Println("North-Rhine Westphalia")
+}
